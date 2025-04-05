4458b3aa52cb60faed775d9ecb8ab154735941bf
Sagar Thapa <isagarthapa@gmail.com>
2025-04-05 23:34:51 +0530

    Login & Logout & Update Profile completed

--- BackEnd/src/index.js
+++ BackEnd/src/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
-import dotenv from 'dotenv'
-import authRoutes from "./routes/auth.route.js";
+import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
+import authRoutes from './routes/auth.route.js';
 import { connectDB } from './lib/db.js';
 
 dotenv.config();
@@ -9,10 +10,11 @@ const app = express();
 const PORT = process.env.PORT;
 
 app.use(express.json());
+app.use(cookieParser());
 
-app.use("/api/auth", authRoutes)
+app.use('/api/auth', authRoutes);
 
 app.listen(5001, () => {
-  console.log("Server is running on PORT:" + PORT);
+  console.log('Server is running on PORT:' + PORT);
   connectDB();
 });
